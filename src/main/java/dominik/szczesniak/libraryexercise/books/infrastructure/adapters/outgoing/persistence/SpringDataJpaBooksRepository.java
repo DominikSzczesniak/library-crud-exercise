9ca1a09043b05c02bd9fa06cb4d3785658ba6149
DominikSzczęśniak <dominikszczesniak128@gmail.com>
2024-04-26 15:59:34 +0200

    removed method in SpringDataJpaBooksRepository

--- src/main/java/dominik/szczesniak/libraryexercise/books/infrastructure/adapters/outgoing/persistence/SpringDataJpaBooksRepository.java
+++ src/main/java/dominik/szczesniak/libraryexercise/books/infrastructure/adapters/outgoing/persistence/SpringDataJpaBooksRepository.java
@@ -9,6 +9,4 @@ public interface SpringDataJpaBooksRepository extends JpaRepository<Book, Long> 
 
 	void deleteById(final Long id);
 
-	boolean existsByAuthorAndTitleAndYear(final String author, final String title, final int year);
-
 }
